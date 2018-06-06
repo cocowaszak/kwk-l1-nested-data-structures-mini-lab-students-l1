b2551a3cb415a4954e11f707674c9e0ebadbb5f9
cocowaszak <colettewaszak@gmail.com>
2018-06-06 22:38:32 +0000

    automatically backed up by learn

---
+++ cli_notes.rb
@@ -0,0 +1,18 @@
+#puts "What's your favorite animal?"
+#answer = gets #takes the input from the terminal with the new line character 
+#puts "I love #{answer}'s!"
+
+#answer = gets.chomp
+#puts "I love #{answer}'s!"
+
+#game_number = "75"
+
+#puts "Please input your first name, Tribute "
+#first_name = gets.chomp
+
+#puts "Please input your last name: "
+#last_name = gets.chomp
+
+#puts "Welcome to the #{game_number}th Hunger Games, #{first_name} #{last_name}"
+
+intger = 0
